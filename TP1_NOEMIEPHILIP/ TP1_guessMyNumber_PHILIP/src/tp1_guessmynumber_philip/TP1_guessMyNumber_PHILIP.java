fd06f09efd2f30e4945706b6656d369c92e8fbe6
phili <phili@10.3.61.220>
2024-09-27 10:32:06 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_NOEMIEPHILIP/ TP1_guessMyNumber_PHILIP/src/tp1_guessmynumber_philip/TP1_guessMyNumber_PHILIP.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_philip;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author phili
+ */
+public class TP1_guessMyNumber_PHILIP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Random generateurAleat = new Random();
+       /* int n = generateurAleat.nextInt(100);
+        int b = generateurAleat.nextInt(100);
+        int c = generateurAleat.nextInt(100);
+        int d = generateurAleat.nextInt(100);
+        int e = generateurAleat.nextInt(100);
+        System.out.println(n);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+*/
+    int n = generateurAleat.nextInt(100);
+    Scanner sc = new Scanner(System.in);
+    
+    
+
+    
+  
+
+
+
+    }
+    
+}
