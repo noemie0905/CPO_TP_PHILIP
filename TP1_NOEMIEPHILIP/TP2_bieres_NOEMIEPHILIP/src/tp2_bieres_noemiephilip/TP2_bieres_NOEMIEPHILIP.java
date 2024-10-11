56646a18b8d8b05128f8a89b5c8baf003ba0c6e0
phili <phili@10.3.61.220>
2024-10-11 09:26:34 +0200

    Création d'un nouveau projet versionné

--- TP1_NOEMIEPHILIP/TP2_bieres_NOEMIEPHILIP/src/tp2_bieres_noemiephilip/TP2_bieres_NOEMIEPHILIP.java
+++ TP1_NOEMIEPHILIP/TP2_bieres_NOEMIEPHILIP/src/tp2_bieres_noemiephilip/TP2_bieres_NOEMIEPHILIP.java
@@ -14,7 +14,19 @@ public class TP2_bieres_NOEMIEPHILIP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        BouteilleBiere uneBiere = new BouteilleBiere() ; 
+        uneBiere.Nom = "Cuvée des trolls"; 
+        uneBiere.degreAlcool = 7.0 ;
+        uneBiere.brasserie="Dubuisson";
+        uneBiere.ouverte=false;
+        uneBiere.lireEtiquette(); 
+
     
+        BouteilleBiere secondeBiere = new BouteilleBiere() ; 
+        secondeBiere.Nom = "Leffe"; 
+        secondeBiere.degreAlcool = 6.6 ;
+        secondeBiere.brasserie="Abbaye de Leffe";
+        secondeBiere.ouverte=false;
+        secondeBiere.lireEtiquette(); 
+    }
 }
