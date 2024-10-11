ebb8c136cb192b26adb94769227feebdcfbabdad
phili <phili@10.3.61.220>
2024-10-11 08:59:43 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_NOEMIEPHILIP/TP2_bieres_NOEMIEPHILIP/src/tp2_bieres_noemiephilip/BouteilleBiere.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_noemiephilip;
+
+/**
+ *
+ * @author phili
+ */
+public class BouteilleBiere { String Nom; double degreAlcool; String brasserie; Boolean ouverte;
+public void lireEtiquette() { 
+System.out.println("Bouteille de " + Nom +" (" + degreAlcool + 
+" degres) \nBrasserie : " + brasserie ) ; 
+}
+}
